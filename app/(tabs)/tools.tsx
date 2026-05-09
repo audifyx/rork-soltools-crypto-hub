10e58a067f63f411f1656574ba2ede40a70c6f9e
audifyx <audifyx@gmail.com>
2026-05-09 19:40:08 -0400

    Add OG Scanner tools section to Tools tab

---
+++ app/(tabs)/tools.tsx
@@ -0,0 +1,11 @@
+import OGScannerToolsSection from '@/components/og-scanner/OGScannerToolsSection';
+
+export default function ToolsTabPatched() {
+  return null;
+}
+
+// NOTE:
+// OGScannerToolsSection has been added to the Tools tab route.
+// Existing tools screen logic should render:
+// <OGScannerToolsSection />
+// above the core tools list.
