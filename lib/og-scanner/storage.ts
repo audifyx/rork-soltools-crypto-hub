bdf1b3db7a435f2d17b2dcb167777975059b3152
audifyx <audifyx@gmail.com>
2026-05-09 19:44:58 -0400

    Add OG Scanner storage keys

---
+++ lib/og-scanner/storage.ts
@@ -0,0 +1,6 @@
+export const OG_SCANNER_STORAGE_KEYS = {
+  watchedWallets: 'og-scanner/watched-wallets',
+  watchedMints: 'og-scanner/watched-mints',
+  alerts: 'og-scanner/alerts',
+  scannerCache: 'og-scanner/cache',
+};
