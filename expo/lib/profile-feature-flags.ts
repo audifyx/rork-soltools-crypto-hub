b8793bb80165cce9e063ed0eee388f92861f4d05
audifyx <audifyx@gmail.com>
2026-05-09 00:05:46 -0400

    Add premium profile feature flags

---
+++ expo/lib/profile-feature-flags.ts
@@ -0,0 +1,4 @@
+export const PREMIUM_PROFILE_ENABLED = true;
+export const GLOWING_BADGES_ENABLED = true;
+export const PROFILE_REPUTATION_ENABLED = true;
+export const TOKEN_GATED_COMMUNITIES_ENABLED = true;
