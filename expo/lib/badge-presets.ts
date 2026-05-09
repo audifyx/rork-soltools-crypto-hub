5759eaa6bae96e80d9ba3ad69349a73a3f025c3e
audifyx <audifyx@gmail.com>
2026-05-09 00:25:05 -0400

    Add badge preset definitions

---
+++ expo/lib/badge-presets.ts
@@ -0,0 +1,22 @@
+export const BADGE_PRESETS = {
+  beta: {
+    label: 'BETA',
+    color: '#C7FF00',
+  },
+  holder: {
+    label: 'HOLDER',
+    color: '#55F5B2',
+  },
+  whale: {
+    label: 'WHALE',
+    color: '#FFD700',
+  },
+  admin: {
+    label: 'ADMIN',
+    color: '#FF6262',
+  },
+  team: {
+    label: 'TEAM',
+    color: '#8B5CFF',
+  },
+};
