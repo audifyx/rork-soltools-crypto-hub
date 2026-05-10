858c70d8dc2fa5e7a6cb8652ea823ff89a5f04c7
audifyx <audifyx@gmail.com>
2026-05-09 20:32:26 -0400

    Add scanner categories

---
+++ lib/scanner/scanner-categories.ts
@@ -0,0 +1,10 @@
+export const SCANNER_CATEGORIES = [
+  'wallet-tracker',
+  'dev-wallets',
+  'holder-analysis',
+  'smart-money',
+  'narratives',
+  'new-pairs',
+  'migrations',
+  'whales',
+];
