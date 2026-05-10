3007425dde0c8f9e68cbfe8b788d35c58bfe7bcd
audifyx <audifyx@gmail.com>
2026-05-09 20:13:04 -0400

    Add optimized FlatList config

---
+++ lib/feed/flatlist-config.ts
@@ -0,0 +1,7 @@
+export const OPTIMIZED_FEED_LIST_PROPS = {
+  initialNumToRender: 10,
+  maxToRenderPerBatch: 5,
+  updateCellsBatchingPeriod: 50,
+  windowSize: 5,
+  removeClippedSubviews: true,
+};
