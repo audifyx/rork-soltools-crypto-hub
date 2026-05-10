b1b434fd8fe79237f8031fe490554e2f3fdaf985
audifyx <audifyx@gmail.com>
2026-05-09 20:31:11 -0400

    Add scanner credit costs

---
+++ lib/credits/credit-costs.ts
@@ -0,0 +1,9 @@
+export const TOOL_CREDIT_COSTS = {
+  tokenScan: 5,
+  walletAnalysis: 25,
+  devWalletAnalysis: 50,
+  narrativeScan: 20,
+  whaleTracking: 15,
+  deepScan: 100,
+  aiNarrativeReport: 150,
+};
