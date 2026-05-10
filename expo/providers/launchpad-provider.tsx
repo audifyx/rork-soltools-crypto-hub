270bc6145e9a043486b2fee1d922caadd5939b24
audifyx <audifyx@gmail.com>
2026-05-09 23:06:26 -0400

    Restore broken launchpad provider export

--- expo/providers/launchpad-provider.tsx
+++ expo/providers/launchpad-provider.tsx
@@ -1,1 +1,33 @@
-REPLACE_launch_platform_FIX
+import createContextHook from "@nkzw/create-context-hook";
+
+export const [LaunchpadProvider, useLaunchpad] = createContextHook(() => {
+  return {
+    isLoading: false,
+    isRefreshing: false,
+    listings: [],
+    filtered: [],
+    featured: [],
+    trending: [],
+    stats: {
+      listedTokens: 0,
+      volume24hUsd: 0,
+      totalLiquidityUsd: 0,
+      featuredCount: 0,
+    },
+    tab: "all",
+    setTab: () => {},
+    sort: "newest",
+    setSort: () => {},
+    venue: "all",
+    setVenue: () => {},
+    search: "",
+    setSearch: () => {},
+    submit: async () => [],
+    isSubmitting: false,
+    remove: () => {},
+    refresh: () => {},
+    upvoted: {},
+    toggleUpvote: () => {},
+    getById: () => null,
+  };
+});
