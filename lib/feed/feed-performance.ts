64d958abfe7c1475ec00f381043a784641516f09
audifyx <audifyx@gmail.com>
2026-05-09 20:12:32 -0400

    Add feed performance helpers

---
+++ lib/feed/feed-performance.ts
@@ -0,0 +1,10 @@
+export const FEED_INITIAL_RENDER = 10;
+export const FEED_BATCH_RENDER = 5;
+export const FEED_WINDOW_SIZE = 5;
+
+export const FEED_LIST_CONFIG = {
+  initialNumToRender: FEED_INITIAL_RENDER,
+  maxToRenderPerBatch: FEED_BATCH_RENDER,
+  windowSize: FEED_WINDOW_SIZE,
+  removeClippedSubviews: true,
+};
